09f6c7d383168d3f7d121310089c36fb30159714
chorsley <cmrhorsley@gmail.com>
2011-08-12 10:12:07 +1000

    Added version reqs to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netgrep',
-    version='0.3.1',
+    version='0.3.2',
     author='CSIRT Foundry / Chris Horsley',
     author_email='chris.horsley@csirtfoundry.com',
     scripts=['bin/netgrep'],
@@ -11,10 +11,10 @@ setup(
     packages=find_packages(),
     #packages=['netgrep'],
     install_requires=[
-        'publicsuffix',
-        'httplib2',
-        'BulkWhois',
-        'adns-python',
+        'publicsuffix>=1.0.0',
+        'httplib2=>0.6.0',
+        'BulkWhois>=0.3.1',
+        'adns-python>=1.2.1',
     ],
     download_url='https://github.com/csirtfoundry/netgrep/tarball/master',
     license='LICENSE.txt',
