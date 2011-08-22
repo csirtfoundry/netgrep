5d49608067b4cfd1c941edf5c3a4c07f7ba4951c
chorsley <cmrhorsley@gmail.com>
2011-08-22 17:15:05 +1000

    Updated version number, added req for argparse

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='netgrep',
-    version='0.3.5',
+    version='0.3.6',
     author='CSIRT Foundry / Chris Horsley',
     author_email='chris.horsley@csirtfoundry.com',
     scripts=['bin/netgrep'],
